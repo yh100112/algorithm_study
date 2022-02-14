90f11856174c09519a76e18c75c34d8265747588
yh100112 <yh100112@naver.com>
2022-02-14 16:21:03 +0900

    정렬 최종정리

--- algorithm/insertion_sort.cpp
+++ algorithm/insertion_sort.cpp
@@ -6,7 +6,7 @@ int main(){
   int i,j,temp;
   int array[10]={1,10,5,8,7,6,4,3,2,9};
 
-  for(i = 0;i < 10; i++){
+  for(i = 0;i < 9; i++){
     j = i;
     while( j >= 0 && array[j] > array[j+1]){
       swap(array[j],array[j+1]);
--- algorithm/test.cpp
+++ algorithm/test.cpp
@@ -13,8 +13,8 @@ void heapify(int i){
   if(heap[child] < heap[child + 1]) child++;
 
   if(heap[parent] < heap[child]){
-    swap(heap[parent], heap[child]);
-    if(child <= n / 2){
+    swap(heap[parent],heap[child]);
+    if(child <= n/2){
       heapify(child);
     }
   }
@@ -23,17 +23,36 @@ void heapify(int i){
 void heapSort(int i){
   int root = 1;
   int child = 2;
-  swap(heap[root], heap[i]);
+
+  swap(heap[root],heap[i]);
 
   do{
     child = root * 2;
-    if(heap[child] < heap[child + 1] && child < i - 1){
-      child++;  
+
+    if(heap[child] < heap[child + 1] && child < i-1){
+      child++;
     }
 
     if(heap[root] < heap[child] && child < i){
-      swap(heap[root], heap[child]);
+      swap(heap[root],heap[child]);
     }
     root = child;
   }while(child < i);
+}
+
+
+int main(){
+  for(int i = n/2; i >= 1; i--){
+    heapify(i);
+  }
+
+  for(int i = n; i > 0; i--){
+    heapSort(i);
+  }
+
+
+  for(int i = 1; i <= n; i++){
+    printf("%d ",heap[i]);
+  }
+  
 }
