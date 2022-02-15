cb47443c10056922e828cac1ecbe0f1ad2bdef0f
yh100112 <yh100112@naver.com>
2022-02-15 17:50:03 +0900

    BFS 알고리즘 개념정리 및 구현

--- algorithm/BFS.cpp
+++ algorithm/BFS.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int number = 7; // 노드의 갯수
 int c[7]; // 방문처리를 위한 배열 생성
-vector<int> a[8];
+vector<int> a[8]; // 이차원 배열
 
 void bfs(int start){
   queue<int> q;
@@ -18,7 +18,7 @@ void bfs(int start){
     printf("%d ",x);
     for(int i = 0; i < a[x].size(); i++){
       int y = a[x][i];
-      if(!c[y]){
+      if(!c[y]){ // 방문하지 않았으면
         q.push(y);
         c[y] = true;
       }
@@ -27,8 +27,45 @@ void bfs(int start){
 }
 
 int main(){
+  //1과 2를 연결
   a[1].push_back(2);
+  a[2].push_back(1);
+  
+  //1과 3을 연결
   a[1].push_back(3);
+  a[3].push_back(1);
+
+  //2와 3을 연결
+  a[2].push_back(3);
+  a[3].push_back(2);
+
+  //2와 4를 연결
+  a[2].push_back(4);
+  a[4].push_back(2);
+  
+  //2와 5를 연결
+  a[2].push_back(5);
+  a[5].push_back(2);
+
+  //3과 6을 연결
+  a[3].push_back(6);
+  a[6].push_back(3);
+
+  //3과 7을 연결
+  a[3].push_back(7);
+  a[7].push_back(3);
+
+  //4와 5를 연결
+  a[4].push_back(5);
+  a[5].push_back(4);
+
+  //6과 7을 연결
+  a[6].push_back(7);
+  a[7].push_back(6);
+
+  //BFS를 수행
+  bfs(1);
+
   
 }
 
