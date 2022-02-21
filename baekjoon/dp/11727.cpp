d7bbabbcbe39fc45b533315aeab956bcbce87eb5
yh100112 <yh100112@naver.com>
2022-02-21 18:03:50 +0900

    baekjoon 11727

---
+++ baekjoon/dp/11727.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int d[1001];
+
+int tile(int n){
+  if(n == 1) return 1;
+  if(n == 2) return 3;
+  if(d[n] != 0) return d[n];
+
+  d[n] = (tile(n-1) + tile(n-2) * 2) % 10007;
+  return d[n];
+}
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  printf("%d",tile(n));
+}
