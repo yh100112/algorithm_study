b57ac21e4e1eebac3d7155b6c20d595ad2d74bd8
yh100112 <yh100112@naver.com>
2022-02-27 16:42:09 +0900

    백준 14852번 dp문제 완료 이틀 걸림

--- baekjoon/dp/14852.cpp
+++ baekjoon/dp/14852.cpp
@@ -3,23 +3,43 @@
 
 using namespace std;
 
-int d[1000001];
+long dp[1000001][2];
 
-int solution(int n){
-    if(n == 0) return 1;
-    if(n == 1) return 2;
-    if(d[n] != 0) return d[n];
+long solution(int n){
+    dp[0][0] = 1;
+    dp[1][0] = 2;
+    dp[2][0] = 7;
+    dp[2][1] = 0;
     
-    d[n] += solution(n-1) * 2 + solution(n-2) * 3;
-    for(int i = n - 3; i >= 0; i = i - 1){
-        d[n] += solution(i) * 2;
+    if(dp[n][0] != 0) return dp[n][0];
+    
+    for(int i = 3; i <= n; i++){
+        dp[i][1] = (dp[i-3][0] + dp[i-1][1]) % 1000000007;
+        dp[i][0] = (3 * dp[i-2][0] + 2 * dp[i-1][0] + 2 * dp[i][1]) % 1000000007;
     }
-    d[n] = d[n] % 1000000007;
-    return d[n];
+
+    return dp[n][0];
 }
 
 int main(){
     int n;
     scanf("%d",&n);
-    printf("%d",solution(n));
+    printf("%ld",solution(n));
 }
+
+/*
+dp[i] = 2 * dp[i - 1] + 3 * dp[i-2]
+        + 2 * dp[i-3]
+        + 2 * dp[i-4]
+        + 2 * dp[i-5]
+        . . .
+        + 2 * dp[0]
+
+이 문제에서는 n이 1000000까지이므로 
+위와 같이 풀면 시간초과 발생 => O(N^2)
+
+O(N)으로 풀 수는 없을까?
+-> dp[i] = 2 * dp[i-1] + 3 * dp[i-2] + 2(dp[i-3] + dp[i-4] + ... dp[0]);
+  dp[i-3]부터 dp[0]까지의 합을 기록하는 DP를 하나 더 만들면 된다!!
+
+*/
