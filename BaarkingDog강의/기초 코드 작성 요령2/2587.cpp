4e56d8298e3602751ec45f56f9e8eec144a3e7e9
yh100112 <yh100112@naver.com>
2022-03-13 22:41:03 +0900

    boj 2587

---
+++ BaarkingDog강의/기초 코드 작성 요령2/2587.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[5];
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int sum = 0;
+  for(int i = 0; i < 5; i++){
+    cin >> arr[i];
+    sum += arr[i];
+  }
+  sort(arr,arr+5);
+  cout << sum / 5 << "\n" << arr[2];
+}
