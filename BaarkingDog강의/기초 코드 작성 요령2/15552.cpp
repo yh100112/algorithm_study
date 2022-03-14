6a0010ed541162723be79c7feab285fa82a046b1
yh100112 <yh100112@naver.com>
2022-03-14 22:26:02 +0900

    boj 15552

---
+++ BaarkingDog강의/기초 코드 작성 요령2/15552.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int t, a, b;
+  cin >> t;
+
+  for(int i = 0; i < t; i++){
+    cin >> a >> b;
+    cout << a + b << "\n";
+  }
+
+}
