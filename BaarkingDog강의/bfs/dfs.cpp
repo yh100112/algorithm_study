5a3f7b5a5983ed60737e3340b1db4972a09ccfcb
yh100112 <yh100112@naver.com>
2022-12-05 22:53:36 +0900

    dfs 개념정리 구현

---
+++ BaarkingDog강의/bfs/dfs.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int number = 7;
+int c[7];
+vector<int> a[8];
+
+void dfs(int x)
+{
+  if (c[x]) return; // 현재 그 노드가 방문한 노드라면 바로 함수 종료
+  c[x] = true;
+  cout << x << ' ';
+  for (int i = 0; i < a[x].size(); ++i) {
+    int y = a[x][i];
+    dfs(y);
+  }
+}
+
+int main() {
+  a[1].push_back(2);
+  a[2].push_back(1);
+
+  a[1].push_back(3);
+  a[3].push_back(1);
+
+  a[2].push_back(4);
+  a[4].push_back(2);
+
+  a[2].push_back(5);
+  a[5].push_back(2);
+
+  a[3].push_back(6);
+  a[6].push_back(3);
+
+  a[3].push_back(7);
+  a[7].push_back(3);
+  
+  a[4].push_back(5);
+  a[5].push_back(4);
+  
+  a[6].push_back(7);
+  a[7].push_back(6);
+
+  dfs(1);
+}
