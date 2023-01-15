b0c066d325f416ed185fb2155b25ac3961ac76db
yh100112 <yh100112@naver.com>
2023-01-15 15:53:41 +0900

    디렉토리 이름 변경

--- Basic100/1086.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main(){
-  long long a,b,c;
-  scanf("%lld %lld %lld",&a,&b,&c);
-
-  printf("%.2f MB",(a*b*c)/pow(2,23));
-}
--- Basic100/1089.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-  int a,d,n;
-  scanf("%d %d %d",&a,&d,&n);
-
-  printf("%d",a + (n-1)*d);
-}
