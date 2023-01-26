89b63077e556ec907fabdbe326a7e4e5d354b233
yh100112 <yh100112@naver.com>
2023-01-26 20:17:31 +0900

    boj 1074 - Z (다시 풀어도 못 품)

--- study/recursion/1074.cpp
+++ study/recursion/1074.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int func(int n, int r, int c) {
+  // if (n == 2) {
+  //   if (x == 0 && y == 0) return 0;
+  //   else if(x == 0 && y == 1) return 1;
+  //   else if(x == 1 && y == 1) return 2;
+  //   return 3;
+  // } -> 내가 한 것 이렇게 base condition을 놓으면 안됨.
   if(n == 0) return 0;
   int half = 1<<(n-1);
   if(r < half && c < half) return func(n-1, r, c);
