bf2db9845f1611c901af24059fceda6bc03cfa82
yh100112 <yh100112@naver.com>
2023-02-10 21:51:31 +0900

    boj 15665 다시 품 - 백트래킹

--- study/backtracking/test.cpp
+++ study/backtracking/test.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
-int main(){
-  int i;
-  for(i = 0; i < 4; i++)
-    cout << "a" << endl;
-  
+int n,m;
+int arr[10];
+bool isused[10];
 
-  cout << i << endl;
+void func(int k){
+  if(k == m){
+    for(int i = 0; i < m; i++){
+      cout << arr[i] << ' ';
+    }
+    cout << "\n";
+    return;
+  }
+
+  for(int i = 1; i <= n; i++){
+    if(!isused[i]){
+      arr[k] = i;
+      isused[i] = 1;
+      func(k+1);
+      isused[i] = 0;
+    }
+  }
+}
+
+
+int main() {
+  cin >> n >> m;
+  func(0);
 }
