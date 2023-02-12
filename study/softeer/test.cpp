a59196c870a9854759598d1beb50b8d52773e740
yh100112 <yh100112@naver.com>
2023-02-12 18:00:54 +0900

    softeer level2 - 전광판 ( 맵 초기화하는법 익혀서 간단하게 수정 )

--- study/softeer/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-  char a = '9';
-  int b = (int)a;
-
-  cout << b << endl;
-}
--- study/softeer/전광판.cpp
+++ study/softeer/전광판.cpp
@@ -1,78 +1,39 @@
 #include<iostream>
-#include<vector>
-#include<utility>
+#include<map>
 using namespace std;
-
-string board[11] = 
+map<char,string> board =
 {
-  "1110111",
-  "0010001",
-  "0111110",
-  "0111011",
-  "1011001",
-  "1101011",
-  "1101111",
-  "1110001",
-  "1111111",
-  "1111011",
-  "0000000" // 아무것도 없는 상태
+  {'0', "1110111"},
+  {'1', "0010001"},
+  {'2', "0111110"},
+  {'3', "0111011"},
+  {'4', "1011001"},
+  {'5', "1101011"},
+  {'6', "1101111"},
+  {'7', "1110001"},
+  {'8', "1111111"},
+  {'9', "1111011"},
+  {' ', "0000000"},
 };
 
 int n; // 테스트 케이스 수
-vector<pair<string,string>> test_case;
-
 int main(int argc, char** argv)
 {	
 	cin >> n;
-	test_case.resize(n);
+  
 	for(int i = 0; i < n; i++){
 		string a,b;
 		cin >> a >> b;
 		int a_len = 5 - a.length();
 		int b_len = 5 - b.length();
-		string chk;
-		chk = "";
-		while(a_len--){
-			chk += ' ';
-		}
-		a = chk + a;
-
-		chk = "";
-		while(b_len--){
-			chk += ' ';
-		}
-		b = chk + b;
+		while(a_len--) a = ' ' + a;
+		while(b_len--) b = ' ' + b;
 		
-		test_case[i].first = a;
-		test_case[i].second = b;
-	}
-
-
-	for(int i = 0; i < n; i++){
-		int cnt = 0;
-		string a = test_case[i].first;
-		string b = test_case[i].second;
-
-		// 5자리 비교
-		for(int j = 0; j < 5; j++){
-			int aa,bb;
-			if(a[j] == ' ')
-				aa = 10;
-			else
-				aa = a[j] - '0';
-			
-			if(b[j] == ' ')
-				bb = 10;
-			else
-				bb = b[j] - '0';
-				
-			for(int k = 0; k < 7; k++){
-				if(board[aa][k] != board[bb][k])
-					cnt++;
-			}
-		}
+    int cnt = 0;
+		for(int j = 0; j < 5; j++)
+			for(int k = 0; k < 7; k++)
+				if(board[ a[j] ][k] != board[ b[j] ][k]) cnt++;
 		cout << cnt << '\n';
 	}
-
 	return 0;
 }
