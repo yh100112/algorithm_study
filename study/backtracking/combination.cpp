14b64751b59e443b9255e6535dd3321f255d487f
yh100112 <yh100112@naver.com>
2023-02-17 20:28:48 +0900

    조합 구하기 - next_permutation

---
+++ study/backtracking/combination.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	vector<int> s{1,2,3,4};
+	vector<int> temp{0,0,1,1};
+
+	do {
+		for (int i = 0; i < s.size(); ++i)
+			if (temp[i] == 0)
+				cout << s[i] << ' ';
+		cout << endl;
+	} while (next_permutation(temp.begin(), temp.end()));
+}
