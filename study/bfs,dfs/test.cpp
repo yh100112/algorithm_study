4ee885a2f01fbc682eac8aaba90c731f0a2f5136
yh100112 <yh100112@naver.com>
2023-02-24 22:05:40 +0900

    boj 1926, 2178 ( bfs ) 복습

--- study/bfs,dfs/test.cpp
+++ study/bfs,dfs/test.cpp
@@ -1,48 +1,40 @@
 #include<iostream>
-#include<utility>
+#include<algorithm>
 #include<queue>
+#include<utility>
 using namespace std;
 #define X first
 #define Y second
-int board[1002][1002];
-int dist[1002][1002];
 int n,m;
+string board[102];
+int dist[102][102];
 int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
 
 int main(){
-  cin >> m >> n;
-  queue<pair<int,int>> Q;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      cin >> board[i][j];
-      if(board[i][j] == 1)
-        Q.push({i,j});
-      if(board[i][j] == 0)
-        dist[i][j] = -1;
+  cin >> n >> m;
+  for(int i=0; i<n; i++)
+    cin >> board[i];
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      dist[i][j] = -1;
     }
   }
+
+  queue<pair<int,int>> Q;
+  Q.push({0,0});
+  dist[0][0] = 1;
   while(!Q.empty()){
     auto cur = Q.front(); Q.pop();
     for(int dir = 0; dir < 4; dir++){
       int nx = cur.X + dx[dir];
       int ny = cur.Y + dy[dir];
       if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist[nx][ny] >= 0) continue;
+      if(board[nx][ny] == '0' || dist[nx][ny] > 0) continue;
       dist[nx][ny] = dist[cur.X][cur.Y] + 1;
       Q.push({nx,ny});
     }
   }
 
-  int ans = 0;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(dist[i][j] == -1){
-        cout << -1;
-        return 0;
-      }
-      ans = max(ans,dist[i][j]);
-    }
-  }
-  cout << ans;
+  cout << dist[n-1][m-1];
 }
