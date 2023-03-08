f0cb6e269fddc868d2809ea4b86b2e237b36f439
yh100112 <yh100112@naver.com>
2023-03-08 22:18:16 +0900

    boj 2206 (벽부수기 - broken을 동일하게 쓰는게 이해가 안감..)

--- study/bfs,dfs/test.cpp
+++ study/bfs,dfs/test.cpp
@@ -1,25 +1,11 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-
-int dist[100002];
-
 int main(){
-  int n,k;
-  cin >> n >> k;
-  fill(dist, dist+100002, -1);
-
-  queue<int> Q;
-  Q.push(n);
-  dist[n] = 0;
-  while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
-    for(auto nx: {cur-1, cur+1, 2 * cur}){
-      if(nx < 0 || nx > 100001) continue;
-      if(dist[nx] != -1) continue;
-      dist[nx] = dist[cur] + 1;
-      Q.push(nx);
-    }
-  }
-  cout << dist[k];
+  int arr[5];
+  for(int i = 0; i < 5; i++)
+    scanf("%1d", &arr[i]);
+  
+  for(int i = 0; i < 5; i++)
+    cout << arr[i] << endl;
 }
