ed9dc1cc31869dcb473a2f3d7065815f7b0cd61b
yh100112 <yh100112@naver.com>
2023-06-14 23:44:49 +0900

    boj 1193 다른 풀이

--- 문제풀이/implementation/1193.cpp
+++ 문제풀이/implementation/1193.cpp
@@ -1,37 +1,59 @@
+// 내 풀이
+// #include<bits/stdc++.h>
+// using namespace std;
+// int X;
+// int cnt, cnt_sum;
+
+// int main(){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cin >> X;
+//     while(true){
+//         if(cnt_sum >= X) break;
+//         cnt++;
+//         cnt_sum += cnt;
+//     }
+//     if(cnt & 1) {
+//         int x = cnt, y = 1;
+//         int st = cnt_sum - cnt + 1;
+//         while(true){
+//             if(st == X) break;
+//             st++;
+//             x--; 
+//             y++;
+//         }
+//         cout << x <<"/" << y << "\n";
+//     }
+//     else {
+//         int x = 1, y = cnt;
+//         int st = cnt_sum - cnt + 1;
+//         while(true){
+//             if(st == X) break;
+//             st++;
+//             x++;
+//             y--;
+//         }
+//         cout << x << "/" << y << "\n";
+//     }
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-int X;
-int cnt, cnt_sum;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> X;
-    while(true){
-        if(cnt_sum >= X) break;
-        cnt++;
-        cnt_sum += cnt;
+    int N;
+    cin >> N;
+
+    int i = 1;
+    while(N > i) {
+        N -= i;
+        i++;
     }
-    if(cnt & 1) {
-        int x = cnt, y = 1;
-        int st = cnt_sum - cnt + 1;
-        while(true){
-            if(st == X) break;
-            st++;
-            x--; 
-            y++;
-        }
-        cout << x <<"/" << y << "\n";
+
+    if(i % 2 == 1) {
+        cout << i + 1 - N << "/" << N << "\n";
     }
     else {
-        int x = 1, y = cnt;
-        int st = cnt_sum - cnt + 1;
-        while(true){
-            if(st == X) break;
-            st++;
-            x++;
-            y--;
-        }
-        cout << x << "/" << y << "\n";
+        cout << N << "/" << i + 1 - N << "\n";
     }
 }
