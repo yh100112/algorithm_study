306a13f66896d4d9a9bdf42fc8e948f5af999606
yh100112 <yh100112@naver.com>
2023-06-17 16:44:22 +0900

    boj 2563 색종이 - 해결법을 간단하게 생각하자! ( 해설 보고 품 ㅜㅜ )

--- 문제풀이/implementation/2563.cpp
+++ 문제풀이/implementation/2563.cpp
@@ -1,26 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,x,y, ret;
-vector<pair<int,int>> v;
+int n,x,y;
+int arr[104][104];
+int cnt;
 
 int main(){
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> x >> y;
-        v.push_back({x,y});
-    }
-    ret = 100 * n;
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        x = v[i].first;
-        y = v[i].second;
-        for(int j = i+1; j < n; j++){
-            int x2 = v[j].first;
-            int y2 = v[j].second;
-            if(abs(x-x2) < 10 && abs(y-y2) < 10) {
-                ret -= (10 - abs(x - x2)) * (10 - abs(y - y2));
+        for(int j = x; j < x+10; j++){
+            for(int k = y; k < y+10; k++){
+                if(arr[j][k] == 0) {
+                    arr[j][k] = 1; 
+                    cnt++;
+                }
             }
         }
     }
-    cout << ret << "\n";
+    cout << cnt << endl;
 }
