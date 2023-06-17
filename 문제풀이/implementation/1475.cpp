8aa40f00e7b06d896c620a2c2c386aa18be30b68
yh100112 <yh100112@naver.com>
2023-06-17 22:07:52 +0900

    boj 1475 방 번호 다른 풀이 - 왜 나이렇게 생각을 못할까...

--- 문제풀이/implementation/1475.cpp
+++ 문제풀이/implementation/1475.cpp
@@ -1,38 +1,65 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// string str;
+// int arr[10];
+
+// int main(){
+//     cin >> str;
+//     int a = stoi(str);
+//     for(int i = 0; i < str.length(); i++){
+//         int num = a / pow(10, str.length() - (i+1));
+//         a = a % (int)pow(10, str.length() - (i+1));
+//         arr[num]++;
+//     }
+//     arr[9] += arr[6];
+//     arr[6] = 0;
+//     int flag = 0;
+//     int cnt = 0;
+//     while(true){
+//         for(int i = 0; i < 10; i++){
+//             if(arr[i] > 0) {
+//                 flag = 1; // 값이 남아있다면 체크
+//                 if(i == 9){
+//                     if(arr[i] >= 2) arr[i] -= 2;
+//                     else arr[i]--;
+//                 }
+//                 else {
+//                     arr[i]--;
+//                 }
+//             }
+//         }
+//         if(flag == 0){
+//             cout << cnt << "\n";
+//             return 0;
+//         }
+//         cnt++;
+//         flag = 0;
+//     }
+// }
+
+// 다른 풀이
 #include<bits/stdc++.h>
 using namespace std;
-string str;
 int arr[10];
 
 int main(){
-    cin >> str;
-    int a = stoi(str);
-    for(int i = 0; i < str.length(); i++){
-        int num = a / pow(10, str.length() - (i+1));
-        a = a % (int)pow(10, str.length() - (i+1));
-        arr[num]++;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    while(n > 0) {
+       arr[n % 10]++;
+       n /= 10; 
     }
-    arr[9] += arr[6];
-    arr[6] = 0;
-    int flag = 0;
-    int cnt = 0;
-    while(true){
-        for(int i = 0; i < 10; i++){
-            if(arr[i] > 0) {
-                flag = 1; // 값이 남아있다면 체크
-                if(i == 9){
-                    if(arr[i] >= 2) arr[i] -= 2;
-                    else arr[i]--;
-                }
-                else {
-                    arr[i]--;
-                }
-            }
-        }
-        if(flag == 0){
-            cout << cnt << "\n";
-            return 0;
-        }
-        cnt++;
-        flag = 0;
+
+    int ret = 1; // 기본 세트수
+    for(int i = 0; i < 10; i++){
+        if(i == 6 || i == 9)
+            continue;
+        ret = max(ret, arr[i]);
     }
+
+    ret = max(ret, (arr[6] + arr[9] + 1) / 2);
+    cout << ret << "\n";
 }
