9ed3988ff99c2590184024a0ce693f06b383ce40
yh100112 <yh100112@naver.com>
2023-06-26 23:20:41 +0900

    boj 3085 테케는 다맞는데 segfault 발생

--- 문제풀이/implementation/3085.cpp
+++ 문제풀이/implementation/3085.cpp
@@ -1,32 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-string ori[52];
+char ori[54][54];
 int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
 
 int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin >> n;
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < n; j++) {
-      cin >> ori[i][j];
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> ori[i][j];
+
+    int mx = -1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            int curX = i;
+            int curY = j;
+            for(int ret = 0; ret < 4; ret++){
+                int nx = curX + dx[ret];
+                int ny = curY + dy[ret];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                if (ori[curX][curY] != ori[nx][ny]) {
+                    swap(ori[curX][curY], ori[nx][ny]);
+                    for(int row = 0; row < n; row++){
+                        int first = ori[row][0];
+                        int cnt = 0;
+                        for(int col = 0; col < n; col++){ // 행 체크
+                            if(first == ori[row][col]){
+                                cnt++;
+                            }
+                            else{
+                                cnt = 1;
+                                first = ori[row][col];
+                            }
+                        }
+                        int first_col = ori[0][row];
+                        int index = row;
+                        int cnt2 = 0;
+                        for(int col = 0; col < n; col++){ // 열 체크
+                            index += n;
+                            if(first_col == ori[index][row]){
+                                cnt2++;
+                            }
+                            else{
+                                cnt2 = 1;
+                                first_col = ori[index][row];
+                            }
+                        }
+                        int m = max(cnt,cnt2);
+                        mx = max(m,mx);
+                    }
+                    swap(ori[curX][curY], ori[nx][ny]);
+                }
+            }
+        }
     }
-  }
-  
-  int mx = -1;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-      int curX = i;
-      int curY = j;
-      for(int ret = 0; ret < 4; ret++){
-        int nx = curX + dx[ret];
-        int ny = curY + dy[ret];
-        if(nx < 0 || nx >= n || ny < 0 || ny >= 0) continue;
-        swap(ori[curX][curY], ori[nx][ny]);
-        // 가로 세로로 카운팅해서 연속하는것 체크
-      }
-    }
-  }
+    cout << mx << "\n";
 }
--- 문제풀이/implementation/test.cpp
+++ 문제풀이/implementation/test.cpp
@@ -5,4 +5,14 @@ int main(){
     string s = "abcde";
     s += 'a';
     cout << s << endl;
+
+    int a[2] = {1,2};
+    swap(a[0],a[1]);
+    for(auto v: a)
+        cout << v << " ";
+    cout << endl;
+    swap(a[0],a[1]);
+    for(auto v: a)
+        cout << v << " ";
+    cout << endl;
 }
