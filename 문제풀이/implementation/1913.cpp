5e7e343b24e23a0487b5619b0b4b8bd4c63be343
yh100112 <yh100112@naver.com>
2023-07-03 21:57:18 +0900

    boj 1913 달팽이 - 풀긴 풀었는데 코드가 너무 하드코딩이라 맘에 안듬

--- 문제풀이/implementation/1913.cpp
+++ 문제풀이/implementation/1913.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int n, m;
 int i,j;
+int ret_x, ret_y;
 int arr[1004][1004];
-int dx[4] = {0,1,0,-1};
+int dx[4] = {0,1,0,-1}; // 우 하 좌 상
 int dy[4] = {1,0,-1,0};
 
 int main(){
@@ -11,25 +12,56 @@ int main(){
     cin.tie(NULL);
     cin >> n >> m;
     i = n/2; j = n/2;
-    int num = 0;
-    int cnt = 0;
-    int k = 3;
-    int idx = 3;
-    int ni = dx[idx]; int nj = dy[idx]; // 첫 시작값 : 상 ( -1,0 )
-    while(num <= n * n){
-        num++; cnt++;
-        arr[i][j] = num;
-        cout << i << " " << j << " " << cnt << endl;
-        if(cnt == k-1) {
-            ni = dx[(idx + 1) % 4]; // 상 -> 우 -> 하 - >좌
-            nj = dy[(idx + 1) % 4];
-            cnt = 0;
+    arr[i][j] = 1;
+    int ni = dx[3]; int nj = dy[3]; // 첫 시작값 : 상 ( -1,0 )
+    int num = 2; int cnt = 1;
+    i += ni; j += nj;
+    int idx = 0;
+    arr[i][j] = num; // 2
+    if (m == 1) {
+        ret_x = n/2; ret_y = n/2;
+    }
+    else if (m == 2) {
+        ret_x = i; ret_y = j;
+    }
+    ni = dx[(idx) % 4]; // 상 -> 우
+    nj = dy[(idx) % 4];
+
+    for(int a = 3; a <= n; a += 2) {
+        while(true){
+            num++; cnt++;
+            i += ni; j += nj;
+            arr[i][j] = num;
+            if (num == m) {
+                ret_x = i; ret_y = j;
+            }
+            if(num == a * a) {
+                break;
+            }
+            if(cnt == a-1) {
+                idx++;
+                ni = dx[idx % 4]; // 상 -> 우 -> 하 - >좌
+                nj = dy[idx % 4];
+                cnt = 0;
+            }
+        }
+        if (a != n) {
+            num++; cnt = 1;
+            i += ni; j += nj;
+            arr[i][j] = num;
+            if (num == m) {
+                ret_x = i; ret_y = j;
+            }
+            idx++;
+            ni = dx[idx % 4]; // 상 -> 우
+            nj = dy[idx % 4];
         }
     }
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             cout << arr[i][j] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
+    cout << ret_x+1 << " " << ret_y+1 << "\n";
 }
