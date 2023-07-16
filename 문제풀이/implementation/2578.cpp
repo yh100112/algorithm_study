3083c798eb24242c51f5f66a2a0a2ca629195c1f
yh100112 <yh100112@naver.com>
2023-07-16 15:04:38 +0900

    boj 2578 - 빙고

--- 문제풀이/implementation/2578.cpp
+++ 문제풀이/implementation/2578.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int arr[5][5]; // 빙고판
-int bingo[13];
+int arr2[5][5]; // 사회자
+int bingo[12];
 
 int main(){
     for(int i = 0; i < 5; i++){
@@ -11,20 +12,41 @@ int main(){
     }
 
     int n;
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
-            cin >> n;
-            for(int a = 1; a <= 5; a++)
-                for(int b = 1; b <= 5; b++)
-                    if(arr[a][b] == n) {
-                        arr[a][b] = 0;
-                        bingo[a]++;
-                        bingo[5 + b]++;
-                        if (a == b) {
-                            bingo[11]++;
-                            if (a == 3) bingo[12]++;
+    int call = 0;
+    int ret = 0;
+    for(int i = 0; i < 5; i++)
+        for(int j = 0; j < 5; j++)
+            cin >> arr2[i][j];
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            call++;
+            int num = arr2[i][j];
+            for(int m = 0; m < 5; m++){
+                for(int n = 0; n < 5; n++){
+                    if(arr[m][n] == num) {
+                        arr[m][n] = -1;
+                        bingo[m]++;     // 0 1 2 3 4
+                        bingo[5 + n]++; // 5 6 7 8 9
+                        if (m == n) {
+                            bingo[10]++;
+                            if (m == 2)
+                                bingo[11]++;
                         }
+                        if (abs(m-n) == 4 || (m == 1 && n == 3) || (m == 3 && n == 1))
+                            bingo[11]++;
                     }
+                }
+            }
+            int cnt = 0;
+            for(int m = 0; m < 12; m++){
+                if(bingo[m] == 5)
+                    cnt++;
+                if(cnt == 3) {
+                    cout << call;
+                    return 0;
+                }
+            }
         }
     }
 }
