96d7a67aa7eb8f6e0a64b7041045bbe1cbdc29ae
yh100112 <yh100112@naver.com>
2023-08-16 23:49:47 +0900

    3-A (boj 15686 치킨배달) - 문제파악

--- 큰돌/2주차/test.cpp
+++ 큰돌/2주차/test.cpp
@@ -1,25 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int n = 10;
-vector<int> adj[n];
-int visited[n];
-int cnt;
-void dfs(int u){
-    visited[u] = 1;
-    cnt++;
-    cout << u << endl;
-    for(int v : adj[u]){
-        if(visited[v] == 0)
-            dfs(v);
-    }
-    cout << u << "로부터 시작된 함수가 종료되었습니다." << endl;
-    return;
+int n, temp;
+vector<int> v;
+bool check(int n){
+	if(n <= 1) return 0;
+	if(n == 2) return 1;
+	if(n % 2 == 0) return 0;
+	for(int i = 2; i * i <= n; i++){
+		if (n % i == 0) return 0;
+	}
+	return 1;
 }
-
+int go(int idx, int sum){
+    cout << idx << " " << sum << endl;
+ 	if(idx == n){
+		return check(sum);
+	}
+	return go(idx + 1, sum + v[idx]) + go(idx + 1, sum);
+    // idx + 1 -> 탐색해 나가면서
+    // sum + v[idx] -> 포함하거나, sum -> 제외시키거나
+}
 int main() {
-    adj[1].push_back(3);
-    adj[2].push_back(3);
-    adj[3].push_back(4);
-    adj[3].push_back(5);
-    dfs(1);
+	cin >> n;
+    v.resize(n);
+	for(int i = 0; i < n; i++)
+        cin >> v[i];
+	cout << go(0,0) << "\n";
+	return 0;
 }
---
+++ 큰돌/3주차/15686(치킨배달).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, m;
+int a[52][52];
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+}
