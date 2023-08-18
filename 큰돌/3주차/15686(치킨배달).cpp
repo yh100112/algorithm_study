b77f90f5ad260083a18759f154a1109d9892b186
김덕용 <yh100112@naver.com>
2023-08-18 10:26:11 +0900

    2-A ( boj 15686 치킨배달) - 풀이보고 직접 혼자 풀어봄

--- 큰돌/3주차/15686(치킨배달).cpp
+++ 큰돌/3주차/15686(치킨배달).cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, ret, a[54][54], result = 987654321;
-vector<vector<int>> chickenList; // 조합들을 저장해놔야 하기 때문에 중첩 vector
+int n, m, a[54][54], visited[54][54], result = INT_MAX;
 vector<pair<int,int>> _home, chicken;
+vector<vector<int>> checkList;
+
 void combi(int start, vector<int> v){
     if(v.size() == m){
-        chickenList.push_back(v);
+        checkList.push_back(v);
         return;
     }
-    for(int i = start + i; i < chicken.size(); i++){
+    for(int i = start + 1; i < chicken.size(); i++){
         v.push_back(i);
         combi(i, v);
         v.pop_back();
@@ -16,7 +17,6 @@ void combi(int start, vector<int> v){
     return;
 }
 
-// 1 : 집, 2 : 치킨집
 int main(){
     cin >> n >> m;
     for(int i = 0; i < n; i++){
@@ -26,8 +26,20 @@ int main(){
             if(a[i][j] == 2) chicken.push_back({i,j});
         }
     }
+
     vector<int> v;
     combi(-1, v);
-    for(vector<int> cList : chickenList){
+    for(auto cList : checkList){ // 3개 고른 조합
+        int ret = 0; // 도시의 치킨 거리
+        for(auto home : _home){
+            int _min = INT_MAX; // 집의 치킨 거리
+            for(auto c : cList){ // 한개의 집과 치킨집들간의 치킨 거리 총 합
+                int h = abs(home.first - chicken[c].first) + abs(home.second - chicken[c].second);                    
+                _min = min(h, _min);
+            }
+            ret += _min;
+        }
+        result = min(ret, result);
     }
+    cout << result << endl;
 }
