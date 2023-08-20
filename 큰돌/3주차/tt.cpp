74dc15e6bbbca6aa642686ac659c2be15f821ee1
yh100112 <yh100112@naver.com>
2023-08-20 17:31:58 +0900

    3-C (boj 16234 인구이동) 풀이보고 품

--- 큰돌/3주차/tt.cpp
+++ 큰돌/3주차/tt.cpp
@@ -1,11 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> adj[6];
+int visited[6];
+
+void dfs(int a){
+    visited[a] = 1;
+    cout << a << "\n";
+    for(auto v : adj[a]){
+        if(visited[v]) continue;
+        dfs(v);
+    }
+    cout << a << "로부터 시작된 함수가 종료되었습니다." << "\n";
+}
 
 int main(){
-    int arr[] = {1,2,3};
-    for(int i = 0; i < 3; i++){
-        for(int j = i+1; j < 3; j++){
-            cout << arr[i] << " " << arr[j] << endl;
-        }
-    }
+	adj[1].push_back(2);
+	adj[1].push_back(3);
+	adj[2].push_back(4);
+	adj[4].push_back(2);
+	adj[2].push_back(5);
+    dfs(1);
 }
