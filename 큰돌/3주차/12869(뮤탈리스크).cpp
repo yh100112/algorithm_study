1ee0d28feba39d423078802e5d830aedbd6cdb21
김덕용 <yh100112@naver.com>
2023-08-21 15:47:11 +0900

    3-E (boj 12869 뮤탈리스크) - 좌표평면 형태가 아닌 bfs문제 풀이보고품

---
+++ 큰돌/3주차/12869(뮤탈리스크).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF = 987654321;
+int n, a[3], visited[64][64][64];
+int _a[6][3] = {
+    {9, 3, 1},
+    {9, 1, 3},
+    {3, 1, 9},
+    {3, 9, 1},
+    {1, 3, 9},
+    {1, 9, 3}
+};
+struct A{
+    int a,b,c;
+};
+
+queue<A> q;
+int solve(int a, int b, int c){
+    visited[a][b][c] = 1;
+    q.push({a,b,c});
+    while(q.size()){
+        int a = q.front().a;
+        int b = q.front().b;
+        int c = q.front().c;
+        q.pop();
+        if(visited[0][0][0]) break;
+        for(int i = 0; i < 6; i++){
+            int na = max(0, a - _a[i][0]); // -를 방지
+            int nb = max(0, b - _a[i][1]);
+            int nc = max(0, c - _a[i][2]);
+            if(visited[na][nb][nc]) continue;
+            visited[na][nb][nc] = visited[a][b][c] + 1;
+            q.push({na, nb, nc});
+        }
+    }
+    return visited[0][0][0] - 1;
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i]; // 체력은 1이상 60이하
+    cout << solve(a[0], a[1], a[2]) << "\n";
+    return 0;
+}
--- 큰돌/3주차/tt.cpp
+++ 큰돌/3주차/tt.cpp
@@ -1,8 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+int a[4] = {1,2,3,4};
+int n = 4, r = 2;
 
-int main(){
-    int a = INFINITY;
-    if(a == INFINITY) cout << "aa" << endl;
+void print(){
+    for(int i = 0; i < r; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
+void makePermutation(int n, int r, int depth){
+    if(depth == r){
+        print();
+        return;
+    }
+    for(int i = depth; i < n; i++){
+        swap(a[i], a[depth]);
+        makePermutation(n,r,depth+1);
+        swap(a[i], a[depth]);
+    }
+    return;
+}
+
+int main(){
+    makePermutation(n,r,0);
+    return 0;
+}
