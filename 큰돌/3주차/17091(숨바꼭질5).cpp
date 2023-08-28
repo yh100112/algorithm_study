9391fc4571795c306237dfe83c82255bef5c4dd4
김덕용 <yh100112@naver.com>
2023-08-28 11:18:58 +0900

    3-I (boj 17091 숨바꼭질5) - 2시간 걸려서 하나하나 다시 파악하며 완벽 이해 성공

--- 큰돌/3주차/17091(숨바꼭질5).cpp
+++ 큰돌/3주차/17091(숨바꼭질5).cpp
@@ -2,22 +2,25 @@
 using namespace std;
 const int max_n = 500000;
 int visited[2][max_n + 4], a, b, ok, turn = 1;
-// visited : 수빈이의 위치를 홀수 짝수 두가지로 나눈 것
+// visited : 수빈이의 위치를 홀수초 짝수초 두가지로 나눈 것
+// visited[0][x] : 짝수초 ( 0초, 2초, 4초, . . .)
+// visited[1][x] : 홀수초 ( 1초, 3초, 5초, . . .)
+
 int main(){
     cin >> a >> b;
     if(a == b){cout << 0 << "\n"; return 0;}
     queue<int> q;
-    visited[0][a] = 1; //  수빈이가 동생 위치에 이미 방문했는지 안했는지를 체크하는 배열 ( 홀,짝으로 나뉨 )
+    visited[0][a] = 1; // 처음 시작은 0초이므로 짝수에 거리가 들어감
     q.push(a);
     while(q.size()){
         b += turn; // 동생 이동
         if(b > max_n) break;
-        // 동생 위치에 수빈이가 이미 방문을 한 경우 체크
-        // turn이 홀수면 홀수 짝수면 짝수 체크 -> 둘다 같아야 만날 수 있음
+        // 동생 위치에 수빈이가 이미 동생이 방문하기 이전에 방문을 한 경우 체크 ( turn이 홀수면 홀수 짝수면 짝수여야 만날 수 있음 )
         if(visited[turn % 2][b]){
             ok = true;
             break;
         }
+
         // bfs에서 턴(거리)마다 구분해서 처리하도록 하기 위해 매번 큐사이즈를 새롭게 설정
         int qSize = q.size();
         for(int i = 0; i < qSize; i++){
@@ -25,11 +28,10 @@ int main(){
             for(int nx : {x + 1, x - 1, x * 2}){
                 if(nx < 0 || nx > max_n || visited[turn % 2][nx]) continue; // overflow체크, 방문했는지 체크
                 
-                // 수빈이가 2초에 방문했으면 다음은 3초에 방문하는 것이므로 turn + 1이 되는 것 -> 홀짝으로 구분하기 떄문
-                // 이해가 안되는데?...
+                // 수빈이가 2초에 방문했으면 다음은 3초에 방문하는 것이므로 turn + 1이 되는 것
                 visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1; 
 
-                // 수빈이와 동생이 같이 가면서 만나는 경우 체크
+                // 수빈이와 동생이 동시에 같은 곳에 도착해 만나는 경우 체크
                 if(nx == b){
                     ok = 1; break;
                 }
