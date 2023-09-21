6d39a4137e1bd9222b56b3cc5f8ed2681079a176
김덕용 <yh100112@naver.com>
2023-09-21 12:36:25 +0900

    boj 3197 백조의호수 retry3

---
+++ 큰돌문제풀이/3주차/retry2/3197.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
+char a[1504][1504];
+int r, c, swanX, swanY, visited_swan[1504][1504], visited_water[1504][1504], day;
+queue<pair<int,int>> swan, water, swan_temp, water_temp;
+
+void Qclear(queue<pair<int,int>>& q){
+    queue<pair<int,int>> temp;
+    swap(q, temp);
+}
+
+void water_melting(){
+    while(water.size()){
+        int y = water.front().first;
+        int x = water.front().second;
+        water.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited_water[ny][nx]) continue;
+            visited_water[ny][nx] = 1;
+            if(a[ny][nx] == '.' || a[ny][nx] == 'L'){
+                water.push({ny,nx});
+            }
+            else if(a[ny][nx] == 'X'){
+                water_temp.push({ny,nx}), a[ny][nx] = '.';
+            }
+        }
+    }
+}
+
+bool move_swan(){
+    while(swan.size()){
+        int y = swan.front().first;
+        int x = swan.front().second;
+        swan.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited_swan[ny][nx]) continue;
+            visited_swan[ny][nx] = 1;
+            if(a[ny][nx] == 'L') return true;
+            else if(a[ny][nx] == 'X') swan_temp.push({ny,nx});
+            else if(a[ny][nx] == '.') swan.push({ny,nx});
+        }
+    }
+    return false;
+}
+
+int main(){
+    cin >> r >> c;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> a[i][j];
+            if(a[i][j] == 'L') swanY = i, swanX = j;
+            if(a[i][j] == '.' || a[i][j] == 'L'){
+                water.push({i,j});
+            }
+        }
+    }
+
+    visited_swan[swanY][swanX] = 1;
+    swan.push({swanY,swanX});
+
+    while(true){
+        if(move_swan()) break;
+        water_melting();
+        water = water_temp;
+        swan = swan_temp;
+        Qclear(water_temp);
+        Qclear(swan_temp);
+        day++;
+    }
+    cout << day << '\n';
+
+}
