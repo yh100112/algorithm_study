19ddfceaea21efa27da391390ae3db0600e78470
김덕용 <yh100112@naver.com>
2023-09-22 10:33:36 +0900

    boj 14620 꽃길 retry3

---
+++ 큰돌문제풀이/3주차/retry2/14620.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF = 987654321;
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
+int n, a[12][12], visited[12][12], ret = INF;
+
+bool check(int y, int x){
+    if(visited[y][x]) return false;
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) return false;
+    }
+    return true;
+}
+
+int setFlower(int y, int x){
+    visited[y][x] = 1;
+    int sum = a[y][x];
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        visited[ny][nx] = 1;
+        sum += a[ny][nx];
+    }
+    return sum;
+}
+
+void eraseFlower(int y, int x){
+    visited[y][x] = 0;
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        visited[ny][nx] = 0;
+    }
+}
+
+void go(int cnt, int sum){
+    if(cnt == 3){
+        ret = min(ret, sum);
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(check(i,j)){
+                go(cnt + 1, sum + setFlower(i,j));
+                eraseFlower(i,j);
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> a[i][j];
+    go(0,0);
+    cout << ret << '\n';
+}
