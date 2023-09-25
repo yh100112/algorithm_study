df3ac282c42e1269c4a0859395557b4fd1f58aa7
김덕용 <yh100112@naver.com>
2023-09-25 16:46:30 +0900

    boj 2529 부등호 retry4

---
+++ 큰돌문제풀이/3주차/retry4/2529.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int k, visited[10];
+char a[10];
+vector<string> v;
+
+bool check(char a, int b, int c){
+    if(a == '>') return b > c;
+    if(a == '<') return b < c;
+    return false;
+}
+
+void go(int idx, string s){
+    if(idx == k + 1){
+        v.push_back(s);
+        return;
+    }
+    for(int i = 0; i <= 9; i++){
+        if(visited[i]) continue;
+        if(idx == 0 || check(a[idx - 1], s[idx - 1] - '0', i)){
+            visited[i] = 1;
+            go(idx + 1, s + to_string(i));
+            visited[i] = 0;
+        }
+    }
+}
+
+int main(){
+    cin >> k;
+    for(int i = 0; i < k; i++)
+        cin >> a[i];
+    go(0, "");
+    sort(v.begin(), v.end());
+    cout << v[v.size() - 1] << '\n';
+    cout << v[0] << '\n';
+}
