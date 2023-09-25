7e1b212160148bdbf8d99efb0b9978db598a43b8
김덕용 <yh100112@naver.com>
2023-09-25 15:35:51 +0900

    boj 4179 불! retry4

---
+++ 큰돌문제풀이/3주차/retry4/4179.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
+const int INF = 987654321;
+int r, c, visited_fire[1004][1004], visited_jh[1004][1004];
+char a[1004][1004];
+queue<pair<int,int>> q_jh, q_fire;
+
+int main(){
+    cin >> r >> c;
+    fill(&visited_fire[0][0], &visited_fire[0][0] + 1004*1004, INF);
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> a[i][j];
+            if(a[i][j] == 'J') {
+                q_jh.push({i,j});
+                visited_jh[i][j] = 1;
+            }
+            if(a[i][j] == 'F') {
+                q_fire.push({i,j});
+                visited_fire[i][j] = 1;
+            }
+        }
+    }
+
+    // fire
+    while(q_fire.size()){
+        int y = q_fire.front().first;
+        int x = q_fire.front().second;
+        q_fire.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited_fire[ny][nx] != INF) continue;
+            if(a[ny][nx] == '#') continue;
+            visited_fire[ny][nx] = visited_fire[y][x] + 1;
+            q_fire.push({ny,nx});
+        }
+    }
+
+    //jh
+    while(q_jh.size()){
+        int y = q_jh.front().first;
+        int x = q_jh.front().second;
+        q_jh.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= r || nx < 0 || nx >= c){
+                cout << visited_jh[y][x] << '\n';
+                return 0; 
+            }
+            if(visited_jh[ny][nx] || a[ny][nx] == '#') continue;
+            if(visited_fire[ny][nx] > visited_jh[y][x] + 1){
+                visited_jh[ny][nx] = visited_jh[y][x] + 1;
+                q_jh.push({ny,nx});
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << '\n';
+    return 0;
+}
