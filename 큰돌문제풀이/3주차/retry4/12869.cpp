199c434df396efafd5af6e4df758f2add2265826
김덕용 <yh100112@naver.com>
2023-09-25 15:50:33 +0900

    boj 12869 뮤탈리스크 retry4

---
+++ 큰돌문제풀이/3주차/retry4/12869.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[3], visited[64][64][64];
+const int attack[6][3] = {
+    {9,3,1},
+    {9,1,3},
+    {3,9,1},
+    {3,1,9},
+    {1,3,9},
+    {1,9,3}
+};
+struct A{
+    int a,b,c;
+};
+queue<A> q;
+
+int bfs(int a, int b, int c){
+    q.push({a,b,c});
+    visited[a][b][c] = 1;
+    while(q.size()){
+        int a = q.front().a;
+        int b = q.front().b;
+        int c = q.front().c;
+        q.pop();
+        if(visited[0][0][0]) return visited[0][0][0] - 1;
+        for(int i = 0; i < 6; i++){
+            int na = max(0, a - attack[i][0]);
+            int nb = max(0, b - attack[i][1]);
+            int nc = max(0, c - attack[i][2]);
+            if(visited[na][nb][nc]) continue;
+            visited[na][nb][nc] = visited[a][b][c] + 1;
+            q.push({na,nb,nc});
+        }
+    }
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];   
+    cout << bfs(a[0], a[1], a[2]) << '\n';
+}
