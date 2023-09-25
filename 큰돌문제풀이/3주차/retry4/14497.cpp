eacd95396419a4c2b2877b54ef5ad74966b629b6
김덕용 <yh100112@naver.com>
2023-09-25 16:23:29 +0900

    boj 14497 주난의 난 retry4

---
+++ 큰돌문제풀이/3주차/retry4/14497.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define x1 aaa
+#define y1 aaaa
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
+int n, m, x1, y1, x2, y2, visited[304][304], cnt;
+char a[304][304];
+queue<pair<int,int>> q;
+
+int main(){
+    cin >> n >> m;
+    cin >> y1 >> x1 >> y2 >> x2;
+    y1--, x1--, y2--, x2--;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+    visited[y1][x1] = 1;
+    q.push({y1,x1});
+
+    while(a[y2][x2] != '0'){
+        cnt++;
+        queue<pair<int,int>> temp;
+        while(q.size()){
+            int y = q.front().first;
+            int x = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+                visited[ny][nx] = cnt;
+                if(a[ny][nx] == '0'){
+                    q.push({ny,nx});
+                }
+                else{
+                    temp.push({ny,nx});
+                    a[ny][nx] = '0';
+                }
+            }
+        }
+        q = temp;
+    }
+    cout << cnt << '\n';
+}
