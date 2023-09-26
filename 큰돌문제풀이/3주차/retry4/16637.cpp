1fb7817aaa85675d746676de908419df8130bac9
김덕용 <yh100112@naver.com>
2023-09-26 12:38:42 +0900

    boj 16637 괄호 추가하기 retry4 ( 완전한 이해가 안 된 것 같음)

---
+++ 큰돌문제풀이/3주차/retry4/16637.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF = -987654321;
+int n, ret = INF;
+string s;
+vector<int> num;
+vector<char> oper;
+
+int cal(char a, int b, int c){
+    if(a == '+') return b + c;
+    if(a == '*') return b * c;
+    if(a == '-') return b - c;
+}
+
+void go(int idx, int sum){
+    if(idx == num.size() - 1){
+        ret = max(ret, sum);
+        return;
+    }
+
+    go(idx + 1, cal(oper[idx], sum, num[idx + 1]));
+    if(idx + 2 < num.size()){
+        int temp = cal(oper[idx + 1], num[idx + 1], num[idx + 2]);
+        go(idx + 2, cal(oper[idx], sum, temp));
+    }
+}
+
+int main(){
+    cin >> n;
+    cin >> s;
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0) num.push_back(s[i] - '0');
+        else oper.push_back(s[i]);
+    }
+    go(0, num[0]);
+    cout << ret << '\n';
+}
