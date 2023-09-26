6e61c393163facac1e627c1e85dfd48c0e9edf45
김덕용 <yh100112@naver.com>
2023-09-26 15:24:54 +0900

    boj 13913 숨바꼭질4 retry4

---
+++ 큰돌문제풀이/3주차/retry4/13913.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define prev aaaa
+const int _max = 100000;
+int n, k, visited[_max + 4], prev[_max + 4];
+queue<int> q;
+vector<int> route;
+
+int main(){
+    cin >> n >> k;
+    q.push(n);
+    visited[n] = 1;
+
+    while(q.size()){
+        int cur = q.front();
+        q.pop();
+        if(cur == k) break;
+        for(auto next : {cur - 1, cur + 1, cur * 2}){
+            if(next < 0 || next > _max) continue;
+            if(visited[next]) continue;
+            visited[next] = visited[cur] + 1;
+            q.push(next);
+            prev[next] = cur;
+        }
+    }
+    cout << visited[k] - 1 << '\n';
+    
+    for(int i = k; i != n; i = prev[i])
+        route.push_back(i);
+    
+    reverse(route.begin(), route.end());
+    cout << n << ' ';
+    for(auto i : route)
+        cout << i << ' ';
+    cout << '\n';
+}
