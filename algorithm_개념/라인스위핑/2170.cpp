740dd640ebf779cc5196dfc542aca70ba5ca3652
김덕용 <yh100112@naver.com>
2023-09-27 11:56:23 +0900

    라인스위핑

---
+++ algorithm_개념/라인스위핑/2170.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> L[1000004];
+int n, from, to, l, r, ret;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> from >> to;
+        L[i] = {from, to};
+    }
+    sort(L, L + n);
+    l = L[0].first;
+    r = L[0].second;
+
+    for (int i = 1; i < n; i++){
+        if(r < L[i].first){
+            ret += (r - l);
+            l = L[i].first;
+            r = L[i].second;
+        }
+        else if(L[i].first <= r && L[i].second > r){
+            r = L[i].second;
+        }
+    }
+    ret += r - l;
+    cout << ret << '\n';
+}
