fd3b79c2782d6cf44e28f8918ee6337581cbc8e2
김덕용 <yh100112@naver.com>
2023-10-06 17:09:51 +0900

    boj 1189 컴백홈 retry5

---
+++ 큰돌문제풀이/3주차/retry5/1189.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
+int r, c, k, ret, visited[7][7];
+char a[7][7];
+
+void go(int y, int x, int cnt){
+    if(y == 0 && x == c-1){
+        if(cnt == k) ret++;
+        return;
+    }
+    
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited[ny][nx]) continue;
+        if(a[ny][nx] == 'T') continue;
+        visited[ny][nx] = 1;
+        go(ny,nx, cnt + 1);
+        visited[ny][nx] = 0;
+    }
+
+}
+
+int main(){
+    cin >> r >> c >> k;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> a[i][j];
+        }
+    }
+    visited[r-1][0] = 1;
+    go(r-1, 0, 1);
+    cout << ret << '\n';
+}
