15e4f2e52e6fd6b7723605b8eb34c2c1ee95132f
김덕용 <yh100112@naver.com>
2023-10-06 14:03:38 +0900

    boj 2109 순회강연 try

--- 큰돌문제풀이/4주차/2109.cpp
+++ 큰돌문제풀이/4주차/2109.cpp
@@ -1,19 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n, p, d, sum;
-vector<priority_queue<int>> v(10004);
+vector<pair<int,int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main(){
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> p >> d;
-        v[d].push(p);
+        v.push_back({d,p});
     }
-    int j = 0;
-    for(int day = 1; day <= 10000; day++){
-        if(v[day].size()){
-            sum += v[day].top();
-        }
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < n; i++){
+        pq.push(v[i].second);
+        if(pq.size() > v[i].first)
+            pq.pop();
+    }
+    while(pq.size()){
+        sum += pq.top();
+        pq.pop();
     }
     cout << sum << '\n';
 }
