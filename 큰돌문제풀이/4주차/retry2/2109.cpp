7b0c022307350b09849ccbd871f83539d61620ec
김덕용 <yh100112@naver.com>
2023-10-10 14:19:14 +0900

    boj 2109 순회강연 retry2

---
+++ 큰돌문제풀이/4주차/retry2/2109.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, d, p, sum;
+vector<pair<int,int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> p >> d;
+        v.push_back({d, p});
+    }
+    sort(v.begin(), v.end());
+    for(int i = 0; i < n; i++){
+        pq.push(v[i].second);
+        if(pq.size() > v[i].first)
+            pq.pop();
+    }
+    while(pq.size()){
+        sum += pq.top();
+        pq.pop();
+    }
+    cout << sum << '\n';
+}
