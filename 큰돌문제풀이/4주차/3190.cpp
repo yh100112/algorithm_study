f54ab66097d13254573b09a901eba8a3de556c72
yh100112 <yh100112@naver.com>
2023-11-04 16:29:29 +0900

    boj 3190 뱀 - dq사용한 풀이

--- 큰돌문제풀이/4주차/3190.cpp
+++ 큰돌문제풀이/4주차/3190.cpp
@@ -1,58 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int dy[] = {0, 1, 0, -1};
-const int dx[] = {1, 0, -1, 0};
-int n, k, l, a[104][104], visited[104][104], ret, aa, bb;
-queue<pair<int,char>> direction;
-queue<pair<int,int>> q, snake;
-
+#define time ff
+#define y1 cc
+int n, k, l, y, x, t, ret, idx, dir = 1;
+int a[104][104], visited[104][104], time;
+char c;
+deque<pair<int, int>> dq;
+vector<pair<int,int>> _time;
+const int dy[] = {-1,0,1,0};
+const int dx[] = {0,1,0,-1};
 int main(){
-    cin >> n; // 보드 크기
-    cin >> k; // 사과 갯수
+    cin >> n >> k;
     for(int i = 0; i < k; i++){
-        cin >> aa >> bb;
-        aa--, bb--;
-        a[aa][bb] = 1; // 사과
+        cin >> y >> x;
+        a[--y][--x] = 1;
     }
-    cin >> l; // 뱀의 방향 변환 횟수
+    cin >> l;
     for(int i = 0; i < l; i++){
-        int t;
-        char d;
-        cin >> t >> d;
-        direction.push({t,d});
+        cin >> t >> c;
+        if(c == 'D') _time.push_back({t, 1});
+        else _time.push_back({t,3});
     }
-    
-    q.push({0,0});
-    snake.push({0,0});
-    visited[0][0] = 1;
 
-    int cur = 0;
-    while(q.size()){
-        ret++;
-        int y = q.front().first;
-        int x = q.front().second;
-        q.pop();
-        int ny = y + dy[cur % 4];
-        int nx = x + dx[cur % 4];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx])
-            break;
-        q.push({ny,nx});
-        snake.push({ny,nx});
-        visited[ny][nx] = 1;
-        if(a[ny][nx] == 0){ // 사과 X
-            auto e = snake.front();
-            snake.pop();
-            visited[e.first][e.second] = 0;
+    dq.push_back({0, 0});
+    while(dq.size()){
+        time++;
+        tie(y, x) = dq.front(); // 머리를 맨 앞에 넣는 이유 -> dq하나로 bfs를 돌기 위해
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if(ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx]) break;
+        if(!a[ny][nx]){
+            visited[dq.back().first][dq.back().second] = 0; // 끝부분 : 꼬리 삭제
+            dq.pop_back();
         }
-        if(direction.front().first == ret){
-            if(direction.front().second == 'L'){
-                cur--;
-                if(cur < 0) cur += 4;   
-            }
-            else
-                cur++;
-            direction.pop();
+        else
+            a[ny][nx] = 0;
+        
+        visited[ny][nx] = 1;
+        dq.push_front({ny,nx}); // 머리 추가
+        if(time == _time[idx].first){
+            dir = (dir + _time[idx].second) % 4; // 이야 이거 참신하다.
+            idx++;
         }
     }
-    cout << ret << '\n';
+
 }
