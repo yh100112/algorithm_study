08ac084033cbbf8fd9d3ed60868d77deeec4fd00
yh100112 <yh100112@naver.com>
2023-11-09 22:46:07 +0900

    boj 2109 순회강연 retry5 ( 성공 )

---
+++ 큰돌문제풀이/4주차/retry5/2109.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, ret;
+vector<pair<int,int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int p, d;
+        cin >> p >> d;
+        v.push_back({d,p}); // day, price
+    }
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < v.size(); i++){
+        pq.push(v[i].second); // price pq에 삽입
+        if(pq.size() > v[i].first){
+            pq.pop();
+        }
+    }
+
+    while(pq.size()){
+        ret += pq.top();
+        pq.pop();
+    }
+    cout << ret << '\n';
+}
