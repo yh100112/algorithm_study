24130772685951817b4b0116e9b8dd917406dec4
yh100112 <yh100112@naver.com>
2023-11-17 22:40:58 +0900

    boj 15662 - 톱니바퀴(2) 성공

--- 큰돌문제풀이/4주차/15662.cpp
+++ 큰돌문제풀이/4주차/15662.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k, a[1004][8], visited[1004], cnt;
+int n, k, a[1004][8], temp[1004][8], visited[1004], cnt;
 string s;
 vector<pair<int,int>> v; // 회전
 
@@ -8,11 +8,16 @@ void go(int num, int dir){
     if(visited[num]) return;
     visited[num] = 1;
 
-    if(dir == -1) rotate(a[num], a[num] + 1, a[num] + 8); // 반시계
-    else rotate(a[num], a[num] + 7, a[num] + 8);          // 시계
+    if(num > 0 && a[num - 1][2] != a[num][6])
+        go(num - 1, dir == -1 ? 1 : -1);
+    if(num < n - 1 && a[num + 1][6] != a[num][2])
+        go(num + 1, dir == -1 ? 1 : -1);
 
-    if(num > 0 && a[num - 1][2] != a[num][6]) go(num - 1, dir == -1 ? 1 : -1);
-    if(num < n - 1 && a[num + 1][6] != a[num][2]) go(num + 1, dir == -1 ? 1 : -1); 
+    if(dir == -1)
+        rotate(a[num], a[num] + 1, a[num] + 8); // 반시계
+    else
+        rotate(a[num], a[num] + 7, a[num] + 8); // 시계
+
 }
 
 int main(){
