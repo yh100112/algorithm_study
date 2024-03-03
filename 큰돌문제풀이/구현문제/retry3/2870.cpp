c48c0259876d7805affd2ffe8c381b40d881bfcc
yh100112 <yh100112@naver.com>
2024-03-03 18:49:54 +0900

    boj 2870 - 수학숙제 retry3

--- 큰돌문제풀이/구현문제/retry3/2870.cpp
+++ 큰돌문제풀이/구현문제/retry3/2870.cpp
@@ -1,49 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
 string s;
-vector<int> v;
+int n;
+vector<string> v;
 
-void check_num(){
+void check(){
     string num = "";
-    for(int i = 0; i < s.length(); ++i){
-        if('0' <= s[i] && s[i] <= '9'){
-            if(s[i] == '0'){
-                if(num.size()){
-                    if(num.back() == '0'){
-                        num.erase(num.length() - 1);
-                    }
-                    else{
-                        num += s[i];
-                    }
-                }
-                else{
-                    num += s[i];
-                }
-            }
-            else{
-                num += s[i];
-            }
-        }
-        else{
-            if(num.size()){
-                v.push_back(stoi(num));
+    for (int i = 0; i < s.length(); ++i) {
+        if ('a' <= s[i] && s[i] <= 'z') {
+            if (num.size()) {
+                while(num.front() == '0')
+                    num.erase(num.begin());
+                if(num.size() == 0)
+                    num += '0';
+                v.push_back(num);
                 num = "";
             }
         }
+        else {
+            num += s[i];
+        }
     }
-    if(num.size())
-        v.push_back(stoi(num));
+    if (num.size()) {
+        while(num.front() == '0')
+            num.erase(num.begin());
+        if(num.size() == 0)
+            num += '0';
+        v.push_back(num);
+    }
 }
 
 int main(){
     cin >> n;
-    for(int i = 0; i < n; ++i){
+    for (int i = 0; i < n; ++i) {
         cin >> s;
-        check_num();
+        check();
     }
 
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), [](string& left, string& right){
+        if (left.size() == right.size())
+            return left < right;
+        else
+            return left.size() < right.size();
+    });
+
     for(auto& i : v)
         cout << i << "\n";
 }
