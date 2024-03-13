546a5bc9dc8ec068881be19d212e3982346977b5
yh100112 <yh100112@naver.com>
2024-03-13 22:38:40 +0900

    수정

--- 2023_기업코테/nsuslab/1.cpp
+++ 2023_기업코테/nsuslab/1.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int cnt = 1;
-string num, db = "1234567890", ret;
+string num, ret;
 
 int main() {
     cin >> num;    
@@ -11,7 +11,7 @@ int main() {
         if (ret.back() == num[i]) continue;
         cnt++;
         num[i] == '0' ? ret += '0' : ret += (num[i] + to_string(num[i] - '0' + 1)); 
-        if (ret.back() != num[i + 1]) {
+        if (num[i] != '0' && ret.back() != num[i + 1]) {
             ret.erase(ret.end() - 1);
             cnt++;
         }
