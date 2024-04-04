c458ebe1725614badf9be90ced39f5766039189d
yh100112 <yh100112@naver.com>
2024-04-04 21:01:46 +0900

    boj 17822 - 답 ( 못 품 )

--- 큰돌문제풀이/4주차/17822.cpp
+++ 큰돌문제풀이/4주차/17822.cpp
@@ -1,110 +1,88 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, t, ret;
-vector<vector<int>> a;
-struct A {
-    int x, d, k; // x의 배수인 원판을 d 방향으로 k칸 회전
-};
-vector<A> v;
+int n, m, T, x, d, k, a[54][54], ret, visited[54][54], y;
+const int dy[] = {-1, 0, 1, 0}; // 인접한 원으로 이동할 때 dx[]는 안 이동함
+const int dx[] = {0, 1, 0, -1}; // 같은 원 안에 있는 인접한 위치로 이동할 때에는 dy[]는 안 이동함
+bool flag = 1;
 
-void spin(int x, int d, int k) {
-    for (int i = 0; i < n; i++) {
-        if ((i + 1) % x != 0) continue;
-        if (d == 0) rotate(a[i].rbegin(), a[i].rbegin() + k, a[i].rend()); // 시계
-        else rotate(a[i].begin(), a[i].begin() + k, a[i].end()); // 반시계
-    }
+void _rotate(int y, int dir, int k) {
+  vector<int> v;
+  for (int i = 0; i < m; i++)
+    v.push_back(a[y][i]); // 현재 원에 있는 숫자들
+  
+  if (dir == 1) rotate(v.begin(), v.begin() + k, v.end()); // 반시계
+  else rotate(v.begin(), v.begin() + m - k, v.end()); // 시계
+
+  for (int i = 0; i < m; i++)
+    a[y][i] = v[i];
 }
 
-bool check() {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (a[i][j]) return true;
-    return false;
+// 인접한 곳 전체 탐색
+void dfs(int y, int x) {
+  // 인접한 원(앞, 뒤 -1 ,1) / 같은 원 내의 인접한 숫자 (우 좌 1, -1) -> 이야 이거 아이디어 기가막힌다;;
+  for (int i = 0; i < 4; i++) {
+    int ny = y + dy[i]; // 인접한 원으로 이동
+    int nx = (x + dx[i] + m) % m; // 같은 원 안에서 옆에 인접한 수 -> ? m 왜 더해주지
+    if (ny < 0 || ny >= n || visited[ny][nx]) continue;
+    if (a[y][x] == a[ny][nx]) {
+      visited[y][x] = visited[ny][nx] = 1;
+      flag = 0; // 인접한 게 있다면 0으로 체크
+      dfs(ny, nx);
+    }
+  }
 }
 
-bool be_closed_erase() {
-    bool check = false;
-    vector<vector<int>> temp = a;
-    for (int i = 0; i < n; i++) {
-        int prev = temp[i][0];
-        int cur;
-        int start = prev;
-        for (int j = 1; j < m; j++) { // 같은 원 내에서 이웃한 수
-            cur = temp[i][j];
-            if (prev == cur) {
-                temp[i][j] = 0;
-                temp[i][j - 1] = 0;
-                check = true;
-            }
-            prev = cur;
-        }
-        if (temp[i][temp[i].size() - 1] == start) {
-            temp[i][0] = 0;
-            temp[i][temp[i].size() - 1] = 0;
-        }
+bool findAdj() {
+  flag = 1;
+  memset(visited, 0, sizeof(visited));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[i][j] == 0 || visited[i][j]) continue;
+      dfs(i, j);
     }
+  }
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      if (visited[i][j]) a[i][j] = 0;
+  return flag;
+}
 
-    vector<vector<int>> temp2 = a;
-    for (int i = 0; i < m; i++) {
-        int prev = temp2[0][i];
-        int cur;
-        for (int j = 1; j < n; j++) { // 서로 다른 원에서의 인접한 위치 ( 같은 인덱스 )
-            cur = temp2[j][i];
-            if (prev == cur) {
-                temp2[j][i] = 0;
-                temp2[j - 1][i] = 0;
-                check = true;
-            }
-            prev = cur;
-        }
+void setAverage() {
+  int sum = 0;
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[i][j] == 0) continue;
+      sum += a[i][j];
+      cnt++;
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (temp[i][j] == 0 || temp2[i][j] == 0)
-                a[i][j] = 0;
-        }
+  }
+  double av = (double)sum / cnt;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(a[i][j] == 0) continue;
+      if((double)a[i][j] > av)      a[i][j]--;
+      else if((double)a[i][j] < av) a[i][j]++;
     }
-    return check;
+  }
 }
 
 int main() {
-    cin >> n >> m >> t; // m : 원판에 적힌 수 개수
-    a.resize(n);
-    for (int i = 0; i < n; i++) {
-        a[i].resize(m);
-        for (int j = 0; j < m; j++)
-            cin >> a[i][j];
-    }
+  cin >> n >> m >> T;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      cin >> a[i][j];
 
-    for (int i = 0; i < t; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        v.push_back({a,b,c});
+  for (int i = 0; i < T; i++) {
+    cin >> x >> d >> k;
+    for (int j = x - 1; j < n; j += x) {  // y의 배수인 원판을 돌린다.
+      _rotate(j, d, k);
     }
+    if (findAdj()) setAverage();
+  }
 
-    for (auto i : v) {
-        int x = i.x;
-        int d = i.d;
-        int k = i.k;
-
-        spin(x, d, k);
-        if (check()) {
-            if (be_closed_erase() == false) {
-                for (int i = 0; i < n; i++) {
-                    int average = accumulate(a[i].begin(), a[i].end(), 0) / a[i].size();
-                    for (int j = 0; j < m; j++) {
-                        if (a[i][j] > average) a[i][j]--;
-                        else if (a[i][j] < average) a[i][j]++;
-                    }
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            ret += a[i][j];
-        }
-    }
-    cout << ret << "\n";
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      ret += a[i][j];
+  cout << ret << "\n";
 }
