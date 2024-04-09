d2d94ffbee5a337b2e7fafabe35f808f3a36efd6
yh100112 <yh100112@naver.com>
2024-04-09 20:48:20 +0900

    boj 1068 - 트리(o)

--- tt.cpp
+++ tt.cpp
@@ -1,17 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, num;
+int n, a, b, root, visited[54];
+vector<int> v[54];
+
+int go(int node) {
+    int flag = 0;
+    int ret = 0;
+    for (auto& next : v[node]) {
+        if (visited[next] || next == b) continue;
+        visited[next] = 1;
+        ret += go(next);
+        flag = 1;
+    }
+
+    if (flag == 0) return 1;
+    return ret;
+}
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> num;
-        long long two = 0;
-        long long five = 0;
-        for (int i = 2; i <= num; i *= 2)
-            two += (num / i);
-        for (int i = 5; i <= num; i *= 5)
-            five += (num / i);
-        cout << min(two, five) << "\n";
+        cin >> a;
+        if (a != -1) v[a].push_back(i);
+        else root = i;
     }
+    cin >> b;
+    visited[root] = 1;
+    if (b == root) cout << 0 << "\n";
+    else cout << go(root) << "\n";;
 }
