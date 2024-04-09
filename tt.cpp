241acee4064d9022c5b015cc518a77d792d99104
yh100112 <yh100112@naver.com>
2024-04-09 21:38:27 +0900

    boj 1629 - 곱셈 (o)

--- tt.cpp
+++ tt.cpp
@@ -1,34 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, a, b, visited[10004], cnt[10004], mx = INT_MIN;
-vector<int> v[10004];
+typedef long long ll;
+ll a, b, c;
 
-int go(int node) {
-    visited[node] = 1;
-
-    int ret = 1; 
-    for (auto& next : v[node]) {
-        if (visited[next]) continue;
-        ret += go(next);
-    }
+ll go(ll a, ll b, ll c) {
+    if (b == 1) return a % c;
+    ll ret = go(a, b / 2, c);
+    ret = (ret * ret) % c;
+    if (b % 2 == 1)
+        ret = ret * a % c;
     return ret;
 }
 
-
 int main() {
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> a >> b;
-        v[b].push_back(a);
-    }
-
-    for (int i = 1; i <= n; i++) {
-        memset(visited, 0, sizeof(visited));
-        cnt[i] = go(i);
-        mx = max(mx, cnt[i]);
-    }
-
-    for (int i = 1; i <= n; i++)
-        if (cnt[i] == mx)
-            cout << i << " ";
+    cin >> a >> b >> c;
+    cout << go(a, b, c) << "\n";
 }
