e4f7098d13620fff0b9442b72b340bf9e929c01e
yh100112 <yh100112@naver.com>
2024-04-09 20:31:03 +0900

    boj 3474 - 교수가 된 현우(x)

--- tt.cpp
+++ tt.cpp
@@ -1,29 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int cnt[2504], used[2504], flag, ret = INT_MIN;
-string s, db = "quack";
+int n, num;
 
 int main() {
-    cin >> s;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] != 'q') continue;
-        int idx = 0;
-        int j = i;
-        while(true) {
-            if (idx == 5 || j == s.length()) break;
-            if (db[idx] == s[j] && !used[j]) {
-                used[j] = 1;
-                idx++;
-            }
-            cnt[j]++;
-            j++;
-        }
-        if (idx != 5) flag = 1;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        long long two = 0;
+        long long five = 0;
+        for (int i = 2; i <= num; i *= 2)
+            two += (num / i);
+        for (int i = 5; i <= num; i *= 5)
+            five += (num / i);
+        cout << min(two, five) << "\n";
     }
-    for (int i = 0; i < s.length(); i++) {
-        if (cnt[i] == 0) flag = 1;
-        ret = max(cnt[i], ret);
-    }
-    if (flag) cout << -1 << "\n";
-    else cout << ret << "\n";
 }
