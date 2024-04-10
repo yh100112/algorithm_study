69bcba814ed6c0309c59289df46efcc48db4a3e8
yh100112 <yh100112@naver.com>
2024-04-10 18:46:31 +0900

    boj 1911 - 흙길 보수하기 (o)

--- tt.cpp
+++ tt.cpp
@@ -1,40 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k, elec[104], visited[104], ret;
-vector<int> tab;
+int n, l, a, b, ret;
+vector<pair<int,int>> v;
 
 int main() {
-    cin >> n >> k;
-    for (int i = 0; i < k; i++)
-        cin >> elec[i];
-    
-    for (int i = 0; i < k; i++) {
-        if (!visited[elec[i]]) {
-            if (tab.size() == n) {
-                if(find(tab.begin(), tab.end(), elec[i]) == tab.end()) {
-                    int last_idx = -1;
-                    int num;
-                    for (int curr_e : tab) {
-                        int idx = INT_MAX;
-                        for (int j = i + 1; j < k; j++) {
-                            if (elec[j] == curr_e) {
-                                idx = j;
-                                break;
-                            }
-                        }
-                        // idx == INT_MAX라면 나중에 사용이 안되는 전자기기임 -> 최우선순위로 뺀다!
-                        if (last_idx < idx) {
-                            last_idx = idx;
-                            num = curr_e;
-                        }
-                    }
-                    tab.erase(find(tab.begin(), tab.end(), num));
-                    visited[num] = 0;
-                    ret++;
-                }
-            }
-            tab.push_back(elec[i]);
-            visited[elec[i]] = 1;
+    cin >> n >> l;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        v.push_back({a,b});
+    }
+    sort(v.begin(), v.end());
+
+    int s = 0;
+    int e = 0;
+    for (int i = 0; i < n; i++) {
+        if (e < v[i].first) { // 널판지 끝이 웅덩이 시작 위치보다 전인 경우
+            int cnt = (v[i].second - v[i].first) / l;
+            int remain = (v[i].second - v[i].first) % l;
+
+            if (remain > 0)
+                cnt++;
+            
+            s = v[i].first; // 널판지 시작 위치
+            e = s + cnt * l; // 널판지 끝 위치
+            ret += cnt;
+        }
+        else if(e < v[i].second) { // 널판지 끝 >= 웅덩이 시작 위치 and 널판지 끝 < 웅덩이 끝 위치
+            int cnt = (v[i].second - e) / l;
+            int remain = (v[i].second - e) % l;
+
+            if (remain > 0)
+                cnt++;
+            
+            e = e + cnt * l;
+            ret += cnt;
         }
     }
     cout << ret << "\n";
