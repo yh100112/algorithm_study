6db707df93b58ec270470487e79f85e14d481897
yh100112 <yh100112@naver.com>
2024-04-10 17:12:57 +0900

    boj 1700 - 멀티탭 스케줄링(x)

--- tt.cpp
+++ tt.cpp
@@ -1,21 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long n, a[100004], visited[100004], ret, s, e;
+int n, k, elec[104], visited[104], ret;
+vector<int> tab;
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
+    cin >> n >> k;
+    for (int i = 0; i < k; i++)
+        cin >> elec[i];
     
-    while(e < n) {
-        if (visited[a[e]]) {
-            ret += (e - s);
-            visited[a[s++]]--;
-        }
-        else {
-            visited[a[e++]]++;
+    for (int i = 0; i < k; i++) {
+        if (!visited[elec[i]]) {
+            if (tab.size() == n) {
+                if(find(tab.begin(), tab.end(), elec[i]) == tab.end()) {
+                    int last_idx = -1;
+                    int num;
+                    for (int curr_e : tab) {
+                        int idx = INT_MAX;
+                        for (int j = i + 1; j < k; j++) {
+                            if (elec[j] == curr_e) {
+                                idx = j;
+                                break;
+                            }
+                        }
+                        // idx == INT_MAX라면 나중에 사용이 안되는 전자기기임 -> 최우선순위로 뺀다!
+                        if (last_idx < idx) {
+                            last_idx = idx;
+                            num = curr_e;
+                        }
+                    }
+                    tab.erase(find(tab.begin(), tab.end(), num));
+                    visited[num] = 0;
+                    ret++;
+                }
+            }
+            tab.push_back(elec[i]);
+            visited[elec[i]] = 1;
         }
     }
-    ret += (e - s) * (e - s + 1) / 2;
     cout << ret << "\n";
 }
