68614b50db06328d437710349f56314fcbad33fd
yh100112 <yh100112@naver.com>
2024-04-10 15:10:30 +0900

    boj 1931 - 회의실배정(o)

--- tt.cpp
+++ tt.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n;
+int n, a, b;
+vector<pair<int,int>> v;
 
 int main() {
-    while(cin >> n) {
-        ll num = 1;
-        int cnt = 1;
-        while(true) {
-            if (num % n == 0) {
-                cout << cnt << "\n";
-                break;
-            }
-            num = (num * 10 + 1) % n;
-            cnt++;
-        }
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        v.push_back({a,b});
     }
+    sort(v.begin(), v.end(), [](pair<int,int>& left, pair<int,int>& right){
+        if (left.second == right.second)
+            return left.first < right.first;
+        return left.second < right.second;
+    });
+
+    int s = v[0].first;
+    int e = v[0].second;
+    int cnt = 1;
+    for (int i = 1; i < v.size(); i++) {
+        if (v[i].first < e) continue;
+        s = v[i].first;
+        e = v[i].second;
+        cnt++;
+    }
+    cout << cnt << "\n";
 }
