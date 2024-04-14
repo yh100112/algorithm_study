06c81f6d12e18db4f4b7bcd6579864a83eb11d61
yh100112 <yh100112@naver.com>
2024-04-14 22:36:27 +0900

    boj 1789

--- tt.cpp
+++ tt.cpp
@@ -1,30 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, ret;
-int isused[40], isused2[40], isused3[40];
-
-void go(int here) {
-    if (here == n) {
-        ret++;
-        return;
-    }
-    
-    for(int x = 0; x < n; x++) {
-        if (isused[x] || isused2[x + here] || isused3[x - here + n - 1]) continue;
-        isused[x] = 1;
-        isused2[x + here] = 1;
-        isused3[x - here + n - 1] = 1;
-        go(here + 1);
-        isused[x] = 0;
-        isused2[x + here] = 0;
-        isused3[x - here + n - 1] = 0;
-    }
-}
+long long s;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    go(0);
-    cout << ret << "\n";
+    cin >> s;
+
+    long long cnt = 1;
+    long long sum = 0;
+    while(true) {
+        sum += cnt++;
+        if (sum > s) break;
+    }
+    if (sum == s) cout << cnt - 1 << "\n";
+    else cout << cnt - 2 << "\n";
 }
