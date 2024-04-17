76f0cfd466ac87f88f98b03454191a809dee1db1
김덕용 <yh100112@naver.com>
2024-04-17 14:59:22 +0900

    boj 11053 - LIS

---
+++ 큰돌문제풀이/5주차/retry1/11053.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[1004], cnt[1004], ret = INT_MIN;
+
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  
+  for (int i = 0; i < n; i++) {
+    int maxValue = 0;
+    for (int j = 0; j < i; j++) {
+      if (a[i] > a[j] && maxValue < cnt[j]) maxValue = cnt[j];
+    }
+    cnt[i] = maxValue + 1;
+    ret = max(ret, cnt[i]);
+  }
+  cout << ret << "\n";
+}
