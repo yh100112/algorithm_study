aca87a4ff90ebc47df6c9579d5cc4ac7f3b18590
yh100112 <yh100112@naver.com>
2024-06-02 17:34:50 +0900

    pgm lv2 - 큰수만들기

---
+++ 추천문제/programmers/큰수만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        
+        // 만들어야 할 문자의 길이만큼
+        for (int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for (int j = start; j <= i + k; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
